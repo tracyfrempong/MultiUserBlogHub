b31643097dfb3792e4ad552e27c3f5f84f31db64
tracy.frempong <tracy.frempong@ashesi.edu.gh>
2024-07-21 12:25:55 +0000

    feat: created initial database schema with user, post, and category tables

---
+++ db/db.js
@@ -0,0 +1,159 @@
+import {
+  Sequelize,
+  STRING,
+  BOOLEAN,
+  INTEGER,
+  TEXT,
+  DATE,
+} from "sequelize";
+
+const sequelize = new Sequelize("multiuser_blog", "root", "root", {
+    host: "localhost",
+    dialect: "mysql",
+    port: 8889,
+    define: {
+        timestamps: false,
+    },
+    logging: false,
+});
+
+const TIMESTAMP = Sequelize.DataTypes.TIMESTAMP;
+
+const Users = sequelize.define("users", {
+  user_id: {
+    type: INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  username: {
+    type: STRING,
+    allowNull: false,
+    unique: true,
+  },
+  email: {
+    type: STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true,
+    },
+  },
+  password: {
+    type: STRING,
+    allowNull: false,
+  },
+  profile_image: {
+    type: STRING,
+    allowNull: true,
+  },
+  bio: {
+    type: STRING,
+    allowNull: true,
+  },
+});
+
+const Categories = sequelize.define("categories", {
+    category_id: {
+      type: INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    category_name: {
+      type: STRING,
+      allowNull: false,
+    },
+    category_description: {
+      type: STRING,
+      allowNull: true,
+    },
+  });
+
+const Posts = sequelize.define("posts", {
+  post_id: {
+    type: INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  user_id: {
+    type: INTEGER,
+    allowNull: false,
+    references: {
+      model: Users,
+      key: "user_id",
+    },
+  },
+  category_id: {
+    type: INTEGER,
+    allowNull: false,
+    references: {
+      model: Categories,
+      key: "category_id",
+    },
+  },
+  title: {
+    type: STRING,
+    allowNull: false,
+  },
+  content: {
+    type: TEXT,
+    allowNull: false,
+  },
+  post_image: {
+    type: STRING,
+    allowNull: true,
+  },
+  published: {
+    type: BOOLEAN,
+    allowNull: false,
+  },
+});
+
+
+// Connect the Users and Posts tables
+const User_Posts = sequelize.define("user_posts", {
+  user_post_id: {
+    type: INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  user_id: {
+    type: INTEGER,
+    allowNull: false,
+    references: {
+      model: Users,
+      key: "user_id",
+    },
+  },
+  post_id: {
+    type: INTEGER,
+    allowNull: false,
+    references: {
+      model: Posts,
+      key: "post_id",
+    },
+  },
+});
+
+// Create tables
+const connect = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+
+    // Forcibly create tables
+    // await sequelize.sync({ force: true });
+    // console.log("All models were synchronized successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+    console.error("Error details:", error.message);
+  }
+};
+
+// Connect to the database
+connect();
+
+export { Users, Posts, Categories, User_Posts };
